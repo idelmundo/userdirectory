1c6372ee98e9436cc9884298ee8b2453d061b0e0
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-15 19:34:55 -0700

    Updates

---
+++ precache-manifest.4bce7144a25362049fe25c3d441538a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a5c931619a513ae9581211a01b714f",
+    "url": "/userdirectory/index.html"
+  },
+  {
+    "revision": "a797068819dafd6b9f5c",
+    "url": "/userdirectory/static/css/main.c3bbba6f.chunk.css"
+  },
+  {
+    "revision": "edb87c19cf3b9c0e02d9",
+    "url": "/userdirectory/static/js/2.f4a3fdea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userdirectory/static/js/2.f4a3fdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a797068819dafd6b9f5c",
+    "url": "/userdirectory/static/js/main.69d3f496.chunk.js"
+  },
+  {
+    "revision": "785d59ea9fa5af4995e3",
+    "url": "/userdirectory/static/js/runtime-main.2f4786bf.js"
+  }
+]);
